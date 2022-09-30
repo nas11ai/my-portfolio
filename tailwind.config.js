0f20833592a543c7a4510aee11990c15c81ad99b
nas11ai <kairupannasai@gmail.com>
2022-09-30 10:32:27 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    screens: {
+      'handphone': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'laptop': '1080px',
+      // => @media (min-width: 1080px) { ... }
+    },
+  }
+}
